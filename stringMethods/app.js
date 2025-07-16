1e2965bf3fc484f1b06d3e5a84f28c207411e40b
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-16 22:15:06 +0900

    Add string methods examples and explanations to app.js

---
+++ stringMethods/app.js
@@ -0,0 +1,49 @@
+/ * eng ko'p ishlatiladigan string metodlari */;
+
+let email = " xamzayevasad442@gmail.com ";
+
+// index of()
+// eng birinchi kelgan stringni indexni topib beradi
+
+// let result = email.indexOf("a");
+// console.log(result);
+
+// LastIndexOf()
+// eng oxirgi kelgan stringni indexni topib beradi
+
+// let result = email.lastIndexOf("a");
+// console.log(result);
+
+// slice()
+// stringni qirqib beradi misol un 0A1s2a3d4b5e6k Agar boshlang'ich qiymatga biror raqam berilsa index bo'yicha o'sha raqamdan keyingi qismnin qirqib beradi
+
+//substr()
+// slicega biroz o'xshab ketadi lekin boshlangich qiymat berilsa va unda keyingi berilgan qiymatgacha kesib beradi misol un Asadbek.substr(1, 5)
+
+// let result = email.substr(8, 4);
+// console.log(result);
+
+// replase()
+// stringni harfni o'zgartirishda ishlatiladi va eng birinchi kelgan xarfni o'zgartirb beradi
+
+// let result = email.replace("s", "X");
+// console.log(result);
+
+//charAt()
+//stringni birinchi xarfini olib beradi
+
+// let result = email.charAt(5);
+// console.log(result);
+
+//trim()
+// stringdagi bo'sh spacelarni olib tashledi
+
+// let result = email.trim();
+// console.log(result.length);
+// console.log(email.length);
+
+//split()
+//barcha stringlarni array ko'rinishda qaytaradi
+
+// let result = email.trim().split("");
+// console.log(result);
