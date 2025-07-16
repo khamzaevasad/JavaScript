296d3d858ae3526e22d5e7c06609978362c88ff9
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-17 06:08:26 +0900

    Add boolean methods and comparison examples to index.js

---
+++ boolean_Comparions/index.js
@@ -0,0 +1,21 @@
+// booleans & Comparisons
+
+// console.log(true, false);
+
+// methods can return booleans
+
+let email = "xamzayevasad442@gmail.com";
+let result = email.includes("!"); //natija false
+// console.log(result);
+
+// Comparisons operation
+
+let age = 36;
+
+console.log(age == 45); // noqattiy tenglik
+console.log(age === "36"); // qattiy tenglik
+console.log(age !== 36); // trueni false falseni truega aylantiradi
+console.log(age > 20); // katta belgisi
+console.log(age >= 20); // katta yoki teng
+console.log(age < 20); // kichik belgisi
+console.log(age <= 20); // kichik yoki teng
