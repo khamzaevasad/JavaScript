16c6b3a0c3fe8799b1d9cd2400da6b9b707477ff
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-16 22:45:14 +0900

    Add number methods examples and explanations to number.js

---
+++ numberMethods/number.js
@@ -0,0 +1,38 @@
+// math operators
+
+//jsda qoldiqli bo'lish % belgisi bilan amalga oshiriladi
+
+// let num = 38 % 5;
+// console.log(num);
+
+// jsda biror sondi darajaga ko'tarish uchun ** dan foydalanamiz
+
+// let num = 15 ** 2;
+// console.log(num);
+
+// order of operation amalarni tartibi
+
+// let num = 10 + (5 - 3) * 2;
+// console.log(num);
+
+// += valueni o'ziga qo'shish
+
+// let people = 20;
+// people += 12;
+// console.log(people);
+
+// -= valueni o'ziga qo'shish
+
+// let people = 20;
+// people -= 12;
+// console.log(people);
+
+// ++ valuega 1 qo'shish -- valueadan 1 ayrish
+
+// let num = 10;
+// num++;
+// num--;
+// console.log(num);
+
+// NaN not a number raqam va string ustida matematik amallar olib borilmaydi
+// console.log(10 * "hello"); natija NaN qaytadi
