9c72f85c1768382058aa1a5acf4fdc6830e4a8e2
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-17 20:58:05 +0900

    Add method examples and explanations to object.js

--- object/object.js
+++ object/object.js
@@ -4,8 +4,21 @@ const myObj = {
   name: "Asadbek",
   age: 25,
   location: "South Korea",
-  1: "computer science and engineering",
+  about: function () {
+    const newObj = {
+      hobby: "swimming",
+      language: "JS",
+    };
+    return `Name: ${this.name} hobby: ${newObj.hobby}`;
+  },
 };
+
+console.log(myObj.about());
+//jsda tsda ichida return bo'lmagan functionlar void function deyiladi
+
+// agarda obj malumotlariga o'zgartirish kiritishni taqiqlash uchun Object.freeze() dan foydalaniladi
+
+// Object.freeze(myObj);
 
 //objectdan 2 xil usulda ma'lumot olinadi 1-dot 2-bracket notation
 
@@ -20,5 +33,26 @@ const myObj = {
 // Objni loop qilish uchun for (let i in {bu yerga obj yoziladi})
 
 for (let i in myObj) {
-  console.log(i);
+  // console.log(myObj[i]); //consolga obj value chiqadi
+  // console.log(i); // consolga obj key chiqadi
 }
+
+//obj valueni o'zgartirish
+// myObj.name = "Akmal";
+
+// obj malumotni delete qilish
+// delete myObj[1];
+
+// objga malumot qo'shish
+
+// myObj.job = "web developer";
+
+// console.log(myObj);
+
+// Obejct.keys() objectni keylarini array qilib beradi
+// Object.values() objectni qiymatini array qilib beradi
+// Obejct.entries() object ichidaga malumotni bitta arrayga tenglaydi va  nested array shakllanadi
+
+// console.log(Object.keys(myObj));
+// console.log(Object.values(myObj));
+// console.log(Object.entries(myObj));
