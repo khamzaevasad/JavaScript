71f72ea3474e743e0e8bb5cacfc9688213deca3c
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-28 19:50:44 +0900

    Add initial synchronous JavaScript code with logging functionality in index.js

---
+++ synchronousJS/index.js
@@ -0,0 +1,15 @@
+// sinxron JS
+
+function boshqaBirIsh() {
+  console.log("tefefonda gaplashish");
+  console.log("coofee ichib bordim");
+}
+
+console.log("tong soat 7da uygonaman");
+console.log("yugurishdan soat 7:30da qaytaman");
+console.log("nonushta qilaman");
+console.log("taxi chaqiraman");
+boshqaBirIsh();
+console.log("ishga ketaman");
+
+//jsda kodlar qatorma qator o'qiladi jsda faqatgina bitta stack yani kod ishlovchi maydon mavjud
