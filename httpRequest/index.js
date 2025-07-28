db0c41152bf1ceb87e40c77746a65308fe8e8d95
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-28 22:56:53 +0900

    Fix getTodos function to correctly parse response and update error message; add todos.json file with sample data

--- httpRequest/index.js
+++ httpRequest/index.js
@@ -1,30 +1,21 @@
-const getToDos = (callback) => {
-  const request = new XMLHttpRequest(); // object serverga malumotni yuborishda kk
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
 
   request.addEventListener("readystatechange", () => {
-    //   console.log(request, request.readyState);
-
     if (request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
-      callback("Malumotni olishni iloji yo", undefined);
+      callback("qandaydir xatolik bor", undefined);
     }
   });
 
-  //open o'zini ichiga 2ta narsa oladi 1-qanday turdegi malumot yuboryabmiz 2-qayerga jonatyabmiz
-
   request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
 
-  // send
-
   request.send();
-
-  // 1 open tayyor
-  // 2 send yuborildi
-  // 3 loading... yuklanyabdi
-  // 4 malumot keldi
 };
-getToDos((err, data) => {
+
+getTodos((err, data) => {
   if (err) {
     console.log(err);
   } else {
