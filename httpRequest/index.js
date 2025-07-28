d8d7b4e11cccfc4977446b86a211fd9a6fb6e713
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-28 22:16:36 +0900

    Refactor getToDos function to use callback for handling response and errors

--- httpRequest/index.js
+++ httpRequest/index.js
@@ -1,24 +1,33 @@
-const request = new XMLHttpRequest(); // object serverga malumotni yuborishda kk
+const getToDos = (callback) => {
+  const request = new XMLHttpRequest(); // object serverga malumotni yuborishda kk
 
-request.addEventListener("readystatechange", () => {
-  //   console.log(request, request.readyState);
+  request.addEventListener("readystatechange", () => {
+    //   console.log(request, request.readyState);
 
-  if (request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);
-  } else if (request.readyState === 4) {
-    console.log("malumotni olishni iloji yo'q");
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("Malumotni olishni iloji yo", undefined);
+    }
+  });
+
+  //open o'zini ichiga 2ta narsa oladi 1-qanday turdegi malumot yuboryabmiz 2-qayerga jonatyabmiz
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
+
+  // send
+
+  request.send();
+
+  // 1 open tayyor
+  // 2 send yuborildi
+  // 3 loading... yuklanyabdi
+  // 4 malumot keldi
+};
+getToDos((err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
   }
 });
-
-//open o'zini ichiga 2ta narsa oladi 1-qanday turdegi malumot yuboryabmiz 2-qayerga jonatyabmiz
-
-request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-
-// send
-
-request.send();
-
-// 1 open tayyor
-// 2 send yuborildi
-// 3 loading... yuklanyabdi
-// 4 malumot keldi
