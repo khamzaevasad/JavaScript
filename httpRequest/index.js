461d38559f0bac179033a18c55e9818b3af9a4dd
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-28 21:49:40 +0900

    Fix XMLHttpRequest handling to log error message on failure

--- httpRequest/index.js
+++ httpRequest/index.js
@@ -3,8 +3,10 @@ const request = new XMLHttpRequest(); // object serverga malumotni yuborishda kk
 request.addEventListener("readystatechange", () => {
   //   console.log(request, request.readyState);
 
-  if (request.readyState === 4) {
+  if (request.readyState === 4 && request.status === 200) {
     console.log(request.responseText);
+  } else if (request.readyState === 4) {
+    console.log("malumotni olishni iloji yo'q");
   }
 });
 
