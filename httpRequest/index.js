b3829bc445c88451584a4c9b537201884e3caefe
khamzaevasad <xamzayevasad442@gmail.com>
2025-07-29 18:54:42 +0900

    Add app.json and data.json files with sample todo data; refactor getTodos function to return a promise

--- httpRequest/index.js
+++ httpRequest/index.js
@@ -1,24 +1,60 @@
-const getTodos = (callback) => {
-  const request = new XMLHttpRequest();
+const getTodos = (resurse) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.addEventListener("readystatechange", () => {
-    if (request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback("qandaydir xatolik bor", undefined);
-    }
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject("malumotni olishni iloji yo'q");
+      }
+    });
+
+    request.open("GET", resurse);
+
+    request.send();
   });
-
-  request.open("GET", "https://jsonplaceholder.typicode.com/todos/");
-
-  request.send();
 };
 
-getTodos((err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
+getTodos("./app.json")
+  .then((data) => {
     console.log(data);
-  }
-});
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//callback hell example
+// getTodos("app.json", (err, data) => {
+//   console.log(data);
+//   getTodos("data.json", (err, data) => {
+//     console.log(data);
+//     getTodos("todos.json", (err, data) => {
+//       console.log(data);
+//     });
+//   });
+// });
+
+//promise
+
+// const internet = false;
+
+// const getData = () => {
+//   return new Promise((resolve, reject) => {
+//     //serverga sorov jonatamiz
+
+//     if (internet) {
+//       resolve("some data");
+//     } else {
+//       reject("some error");
+//     }
+//   });
+// };
+// getData()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
